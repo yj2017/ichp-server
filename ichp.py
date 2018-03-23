975258cf5900f82f6ce78c3f4a6ce1e217f5ed5f
yangjin <1424783824@qqcom>
2018-03-23 22:24:24 +0800

    ..

--- ichp.py
+++ ichp.py
@@ -1260,5 +1260,5 @@ def GetCommComm():
         return decodeStatus(8)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=True)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0',debug=True)
