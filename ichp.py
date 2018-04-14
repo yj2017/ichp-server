e1691316a8d7bff1a9295e57ce66a89cd585449e
yangjin <1424783824@qqcom>
2018-04-14 11:38:55 +0800

    ..

--- ichp.py
+++ ichp.py
@@ -1393,26 +1393,26 @@ def recommendRec():
             listRec = cursor.fetchall()
             recL = []
             app.logger.debug(cursor.rowcount)
-            if cursor.rowcount > 0 :
+            if cursor.rowcount > 0 and cursor.rowcount <= 2:
                 # 返回两条推荐记录
                 for row in range(cursor.rowcount):
                     record = Record(listRec[row][0], listRec[row][1], listRec[row][2], listRec[row][3], listRec[row]
                                     [4], listRec[row][5], listRec[row][6], listRec[row][7], listRec[row][8], listRec[row][9], listRec[row][10])
                     recL.append(record)
                     app.logger.debug(recL)
-            # elif cursor.rowcount > 2:
-            #     for row in range(cursor.rowcount):
-            #         if listRec[row][5] == addr:  # 地点
-            #             record = Record(listRec[row][0], listRec[row][1], listRec[row][2], listRec[row][3], listRec[row]
-            #                             [4], listRec[row][5], listRec[row][6], listRec[row][7], listRec[row][8], listRec[row][9], listRec[row][10])
-            #             recL.append(record)
-            #     if recL.count > 2:
-            #         recL = recL[:2]
-            #     else:
-            #         for row in range(2):
-            #             record = Record(listRec[row][0], listRec[row][1], listRec[row][2], listRec[row][3], listRec[row]
-            #                             [4], listRec[row][5], listRec[row][6], listRec[row][7], listRec[row][8], listRec[row][9], listRec[row][10])
-            #             recL.append(record)
+            elif cursor.rowcount > 2:
+                for row in range(cursor.rowcount):
+                    if listRec[row][5] == addr:  # 地点
+                        record = Record(listRec[row][0], listRec[row][1], listRec[row][2], listRec[row][3], listRec[row]
+                                        [4], listRec[row][5], listRec[row][6], listRec[row][7], listRec[row][8], listRec[row][9], listRec[row][10])
+                        recL.append(record)
+                if len(recL) > 2:
+                    recL = recL[:2]
+                else:
+                    for row in range(2):
+                        record = Record(listRec[row][0], listRec[row][1], listRec[row][2], listRec[row][3], listRec[row]
+                                        [4], listRec[row][5], listRec[row][6], listRec[row][7], listRec[row][8], listRec[row][9], listRec[row][10])
+                        recL.append(record)
             cursor.close()
             return json.dumps({"msg": "successfully", "code": 0, "data": recL}, default=lambda obj: obj.__dict__, ensure_ascii=False)
         except Exception as de:
