43554aef67b3e4702546b7baf96c269e7d09a55a
yangjin <1424783824@qqcom>
2018-04-26 14:34:12 +0800

    ..

--- ichp.py
+++ ichp.py
@@ -266,14 +266,18 @@ def ModifySign():
 # allowed file type
 ALLOWED_EXTENSIONS = set(
     ['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'docx', 'doc',
-     'WMV', 'ASF', 'AVI',  'AVS', 'FLV', 'MKV', 'MOV', '3GP', 'MP4', 'MPG', 'MPEG', 'DAT', 'OGM', 'VOB', 'RM', 'RMVB', 'TS', 'TP', 'IFO', 'NSV'
+     'WMV', 'ASF', 'AVI',  'AVS', 'FLV', 'MKV', 'MOV', '3GP', 'MP4','MPG', 'MPEG', 'DAT', 'OGM', 'VOB', 'RM', 'RMVB', 'TS', 'TP', 'IFO', 'NSV'
      'mp3', 'AAC', 'WAV', 'WMA', 'CDA', 'FLAC', 'M4A', 'MID', 'MKA', 'MP2', 'MPA', 'MPC', 'APE', 'OFR', 'OGG', 'RA', 'WV', 'TTA', 'AC3', 'DTS'
      ]
 )
 
 
 def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+    filename_temp=filename.upper()
+    ALLOWED_EXTENSIONS_temp=set()
+    for allow in ALLOWED_EXTENSIONS:
+        ALLOWED_EXTENSIONS_temp.add(allow.upper())
+    return '.' in filename_temp and filename_temp.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS_temp
 
 
 @app.route('/upload', methods=['POST'])
